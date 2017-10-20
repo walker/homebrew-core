6a6b7fd0765680448170e5a0f963a762a2add1a9
Walker Hamilton <hey@walkerhamilton.com>
2017-10-20 17:17:30 -0500

    Geogig 1.2.0, new formula

---
+++ Formula/geogig.rb
@@ -0,0 +1,21 @@
+class Geogig < Formula
+  desc "Enable distributed versioning of geospatial data in a git-like context"
+  homepage "http://geogig.org"
+  url "http://download.locationtech.org/geogig/geogig-1.2.0.zip"
+  sha256 "c438d97d8978a9d4985bd4f2ba3fff481b7956bfc4bd986ee3c79c6c07cd4f34"
+
+  bottle :unneeded
+
+  depends_on :java => ["1.8+"]
+
+  def install
+    bin.install "bin/geogig"
+    prefix.install "libexec"
+    prefix.install "misc"
+  end
+
+  test do
+    system "#{bin}/geogig", "init"
+    system "#{bin}/geogig", "status"
+  end
+end
